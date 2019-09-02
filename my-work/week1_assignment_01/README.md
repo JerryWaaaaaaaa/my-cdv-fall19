ebd5caba55c17c8e3c730e0b9d10fe47e4779ffa
Jerry <zw1141@nyu.edu>
2019-09-02 22:04:30 +0800

    week1 assignment1

---
+++ my-work/week1_assignment_01/README.md
@@ -0,0 +1,10 @@
+# Week1 Assignment
+
+This is my week1 assignment.
+The detailed information about this assignment can be found [here](https://github.com/leoneckert/cdv-fall19/tree/master/coding-foundation).
+
+## What I've learnt
+
+* How to use `addEventListener`.
+* How to get the value of an input element.
+* How to draw a square using `createElement`.
--- my-work/week1_assignment_01/script.js
+++ my-work/week1_assignment_01/script.js
@@ -3,17 +3,25 @@
 
 function createSquare(){
 
+  // clear the square-drawing area
   document.getElementById("square_container").innerHTML = " ";
 
+  // get the user input
   var squareNum = document.getElementById("square_number").value;
 
+  // draw the squares based on the user input
   for(var i = 0; i < squareNum; i ++){
+    // create a div element
     var square = document.createElement("DIV");
+    // add a class name to the div
     square.className = "square_item";
+    // to test
     console.log("square created!");
+    // append the newly created div to the "square_container" div as its child
     document.getElementById("square_container").appendChild(square);
   }
 
 }
 
+// add an eventListener to the button
 document.getElementById("create_square").addEventListener("click", createSquare);
